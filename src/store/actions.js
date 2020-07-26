894a6a9fce586ee874dcd59ef565093c73cae4b1
jk <webmaster@jkalasz.pl>
2020-07-26 02:14:24 +0200

    download story

--- src/store/actions.js
+++ src/store/actions.js
@@ -5,3 +5,10 @@ export const getAllStories = ({ commit }) => {
         commit('receiveStories', stories)
     })
 };
+
+export const getStory = ({ commit }, id) => {
+    api.getStory(id, (nodes) => {
+        nodes.story_id = id;
+        commit('receiveStory', nodes);
+    })
+};
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
+import {getStory} from "./actions";
+import store from './index'
 
 export default {
     receiveStories(state, stories) {
         Vue.set(state.stories, 'stories' ,stories);
+        stories.forEach(story => {
+            getStory(store, story.story_id)
+        })
+    },
+    receiveStory(state, nodes) {
+        Vue.set(state.stories, nodes.story_id, nodes);
     }
 }
