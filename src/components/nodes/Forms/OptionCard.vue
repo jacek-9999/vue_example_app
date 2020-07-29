18a6bb8d8d6eda7681d3f37690e52efcedca3c89
jk <webmaster@jkalasz.pl>
2020-07-29 06:46:37 +0200

    empty modals

--- src/components/nodes/Forms/OptionCard.vue
+++ src/components/nodes/Forms/OptionCard.vue
@@ -1,4 +1,5 @@
 <template>
+    <div>
     <b-card-group deck>
         <b-card v-for="item in cardData" :key="item.id" :item="item">
             <b-card-text>
@@ -6,24 +7,110 @@
             </b-card-text>
             <hr>
             <div class="d-flex justify-content-center">
-                <b-button variant="warning">
+                <b-button v-b-modal.unlink-option-modal variant="warning">
                     <b-icon icon="x-circle-fill"></b-icon>
                     unlink option
                 </b-button>
             </div>
         </b-card>
         <b-card>
-               <b-button variant="info">
+               <b-button v-b-modal.create-option-modal variant="info">
                    <b-icon icon="plus"></b-icon>
                    Create new option
                 </b-button>
             <hr>
-              <b-button variant="info">
+              <b-button v-b-modal.assign-option-modal variant="info">
                    <b-icon icon="link"></b-icon>
                    Assign to other option
                 </b-button>
         </b-card>
     </b-card-group>
+         <b-modal
+                 ref="unlink-option-modal"
+                 hide-footer id="unlink-option-modal"
+                 title="Unlink Option Confirmation"
+         >
+             <div class="d-block text-center">
+                 <h5>
+<!--              <h5 v-if="story_prepared_to_delete">-->
+                      <hr>
+                      Title:
+                      <hr>
+                      Id:
+                      <hr>
+                      <hr>
+                  </h5>
+             </div>
+             <div class="d-flex justify-content-between">
+                 <b-button class="" variant="info" @click="hideModal">
+                     <b-icon icon="arrow-left"></b-icon>Back
+                 </b-button>
+                 <b-button class="" variant="danger">
+    <!--             <b-button class="" variant="danger" @click="deleteStory">-->
+                     <b-icon icon="x-circle-fill"></b-icon>
+                     Unlink
+                 </b-button>
+             </div>
+         </b-modal>
+
+         <b-modal
+                 ref="create-option-modal"
+                 hide-footer id="create-option-modal"
+                 title="Create Option"
+         >
+             <div class="d-block text-center">
+                 <h5>
+<!--              <h5 v-if="story_prepared_to_delete">-->
+                      <hr>
+                      Title:
+                      <hr>
+                      Id:
+                      <hr>
+                      <hr>
+                  </h5>
+             </div>
+             <div class="d-flex justify-content-between">
+                 <b-button class="" variant="danger" @click="hideModal">
+                     <b-icon icon="arrow-left"></b-icon>Back
+                 </b-button>
+                 <b-button class="" variant="info">
+    <!--             <b-button class="" variant="danger" @click="deleteStory">-->
+                     <b-icon icon="plus"></b-icon>
+                     Create
+                 </b-button>
+             </div>
+         </b-modal>
+
+         <b-modal
+                 ref="assign-option-modal"
+                 hide-footer id="assign-option-modal"
+                 title="Assign Option"
+         >
+             <div class="d-block text-center">
+                 <h5>
+<!--              <h5 v-if="story_prepared_to_delete">-->
+                      <hr>
+                      Title:
+                      <hr>
+                      Id:
+                      <hr>
+                      <hr>
+                  </h5>
+             </div>
+             <div class="d-flex justify-content-between">
+                 <b-button class="" variant="danger" @click="hideModal">
+                     <b-icon icon="arrow-left"></b-icon>Back
+                 </b-button>
+                 <b-button class="" variant="info">
+    <!--             <b-button class="" variant="danger" @click="deleteStory">-->
+                     <b-icon icon="link"></b-icon>
+                     Assign
+                 </b-button>
+             </div>
+         </b-modal>
+
+
+    </div>
 </template>
 <script>
 export default {
@@ -34,6 +121,9 @@ export default {
     components: {
     },
     methods:{
+        hideModal() {
+            this.$refs['unlink-option-modal'].hide()
+        },
     },
     computed: {
     }
